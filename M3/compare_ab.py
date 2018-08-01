5d2d27701e6974c1f1495abf4e5d0e635a6712f5
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-01 14:33:01 +0530

    compare

---
+++ M3/compare_ab.py
@@ -0,0 +1,9 @@
+"""compare"""
+I1 = input("enter varA")
+I2 = input("enter varB")
+if len(I1) > len(I2):
+    print("bigger")
+if len(I1) == len(I2):
+    print("equal")
+if len(I1) < len(I2):
+    print("smaller")
